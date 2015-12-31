799a69082a681f55bc03dd665795c93abae6704d
Landon Fuller <landonf@plausible.coop>
2015-12-31 15:54:37 -0700

    Initial project

---
+++ ccmach/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ccmach
+//
+//  Created by Landon Fuller on 12/31/15.
+//  Copyright (c) 2015 Landon Fuller. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
