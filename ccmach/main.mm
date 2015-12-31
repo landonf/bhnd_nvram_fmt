052628eaa9d3b51335c22c8b1f80d9f426f4ce3c
Landon Fuller <landonf@plausible.coop>
2015-12-31 16:02:34 -0700

    Switch to C++, add ObjectDoc

---
+++ ccmach/main.mm
@@ -0,0 +1,16 @@
+//
+//  main.mm
+//  ccmach
+//
+//  Created by Landon Fuller on 12/31/15.
+//  Copyright (c) 2015 Landon Fuller. All rights reserved.
+//
+
+#include <stdio.h>
+#import <ObjectDoc/ObjectDoc.h>
+#import <ObjectDoc/PLClang.h>
+
+int main (int argc, const char *argv[]) {
+    printf("hello world\n");
+    return (0);
+}
