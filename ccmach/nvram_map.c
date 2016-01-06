48df8f261864aaa9773a7b8915dc98b788302607
Landon Fuller <landonf@plausible.coop>
2016-01-06 12:44:22 -0700

    Clean up nvram struct definitions

--- ccmach/nvram_map.c
+++ ccmach/nvram_map.c
@@ -9,22 +9,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdint.h>
-#include "nvram_map.h"
-
-
-const struct bhnd_nvram_var nvram_vars[] = {
-    BHND_NVRAM_VAR(poodle, UINT, SFMT_HEX, VF_DFLT, 0,
-                   BHND_SPROM_MAPPING(REV_GTE(1),
-                                      BHND_SPROM_VAL_U32(0xFFEE)
-                                      )
-                   ),
-    BHND_NVRAM_VAR(puddles, UINT, SFMT_HEX, VF_ARRAY, 5,
-                   BHND_SPROM_MAPPING(REV_EQ(1), BHND_SPROM_SPARSE_VAL(
-                                                                       {0xFFEE, sizeof(uint32_t), UINT32_MAX, 0},
-                                                                       {0xFFEE, sizeof(uint32_t), UINT32_MAX, 0})
-                                      )
-                   )
-};
+#include "../m.h"
 
 int main (int argc, char * const argv[]) {
 	uint16_t spromver = 256;
